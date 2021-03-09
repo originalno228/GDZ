b3581a951d80f23438a735b5ec38fa56eec76170
originalno228 <70958298+originalno228@users.noreply.github.com>
2021-03-09 15:00:58 +0400

    Add files via upload

---
+++ Продукты.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Продукты
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.Poisk = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(368, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 71);
+            this.button2.TabIndex = 40;
+            this.button2.Text = "Найти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Poisk
+            // 
+            this.Poisk.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Poisk.Location = new System.Drawing.Point(122, 9);
+            this.Poisk.Name = "Poisk";
+            this.Poisk.Size = new System.Drawing.Size(167, 29);
+            this.Poisk.TabIndex = 39;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Cursor = System.Windows.Forms.Cursors.No;
+            this.label14.Location = new System.Drawing.Point(5, 9);
+            this.label14.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(90, 24);
+            this.label14.TabIndex = 38;
+            this.label14.Text = "Продукт:";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.No;
+            this.label6.Location = new System.Drawing.Point(5, 43);
+            this.label6.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 24);
+            this.label6.TabIndex = 49;
+            this.label6.Text = "Бюджет:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // price
+            // 
+            this.price.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.price.Location = new System.Drawing.Point(122, 48);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(167, 29);
+            this.price.TabIndex = 50;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel1.Location = new System.Drawing.Point(3, 88);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1011, 453);
+            this.panel1.TabIndex = 51;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(629, 685);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 24);
+            this.label4.TabIndex = 57;
+            this.label4.Text = "Цена";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(28, 717);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 24);
+            this.label8.TabIndex = 52;
+            this.label8.Text = "Томат";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(837, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 24);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "тут время лол";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.No;
+            this.label2.Location = new System.Drawing.Point(673, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 24);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "Проект запущен:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Cursor = System.Windows.Forms.Cursors.No;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(701, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 26);
+            this.label3.TabIndex = 54;
+            this.label3.Text = "Сегодня красная среда!";
+            // 
+            // Продукты
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 546);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Poisk);
+            this.Controls.Add(this.label14);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Продукты";
+            this.Text = "Семерочка";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Poisk;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Продукты.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+
+    public struct Food
+    {
+        public PictureBox picture;
+        public Label labelPrice;//цена
+        public Label labelName;//название на форме (отоброжается)
+        public string name;
+        public string str;
+        public int price;
+        public string category;
+
+    }
+
+    public partial class Продукты : Form
+    {
+        Food[] eda = new Food[500];
+        public Продукты()
+        {
+            InitializeComponent();
+
+
+            //eda[0].picture = pictureBox1;
+            //("../../../Продукты/бургер.txt");
+            //eda[0].label2 = "a";
+            eda[0].name = "овощи";
+            eda[0].price = 100;
+            
+
+            eda[1].name = "фастфуд";
+            eda[1].price = 50;
+
+            eda[2].name = "Огурец";
+            eda[2].price = 1000;
+            eda[0].category = "фрукт";
+
+            eda[3].name = "Томат";
+            eda[3].price = 9;
+            eda[0].category = "фрукт";
+
+            eda[4].name = "Мороженное";
+            eda[4].price = 75;
+
+            eda[5].name = "Банан";
+            eda[5].price = 4;
+            eda[5].category = "фрукт";
+
+            eda[6].name = "Груша";
+            eda[6].price = 4;
+            eda[6].category = "фрукт";
+
+            eda[7].name = "Кокос";
+            eda[7].price = 400;
+            eda[7].category = "фрукт";
+
+            eda[8].name = "Яблоко";
+            eda[8].price = 400;
+            eda[8].category = "фрукт";
+
+            //время
+            label1.Text = DateTime.Now.ToString("");
+
+            int x = 10;
+            int y = 10;
+            for (int i = 0; i < 9; i++)
+            {
+                //--------------Картинка--------------------------------------
+                eda[i].picture = new PictureBox();
+                eda[i].picture.Location = new Point(x, y);
+                eda[i].picture.SizeMode = PictureBoxSizeMode.Zoom;
+                eda[i].picture.Tag = eda[i].name;
+
+                eda[i].picture.Size = new Size(150, 150);
+                try//попытка загрузить картинку либо jpj либо png либо до связи
+                {
+                    eda[i].picture.Load("../../../Продукты/" + eda[i].name + ".jpg");
+                }
+                catch (Exception)
+                {
+                    try
+                    {
+                        eda[i].picture.Load("../../../Продукты/" + eda[i].name + ".png");
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message); }
+                }
+                eda[i].picture.Click += new EventHandler(pictureBox1_Click);
+                panel1.Controls.Add(eda[i].picture);
+
+                //--------------Цена--------------------------------------
+
+                eda[i].labelPrice = new Label();
+                if (eda[i].price <100)
+                {
+                    eda[i].labelPrice.ForeColor = Color.Red;
+                }
+                
+                eda[i].labelPrice.Location = new Point(eda[i].picture.Location.X + 130, eda[i].picture.Location.Y + 150);
+                //eda[i].labelPrice.Location = new Point(eda[i].picture.Location.X, eda[i].picture.y);
+                //eda[i].label.Location = new Point(300 * i, 200);
+
+                eda[i].labelPrice.Size = new Size(50, 30);
+                eda[i].labelPrice.Text = eda[i].price.ToString();
+                panel1.Controls.Add(eda[i].labelPrice);
+
+                //-----------------название----------------------------------------------
+
+                eda[i].labelName = new Label();
+                eda[i].labelName.Location = new Point(x + 20, y + 150);
+
+                eda[i].labelName.Size = new Size(100, 30);
+                eda[i].labelName.Text = eda[i].name;
+                panel1.Controls.Add(eda[i].labelName);
+
+                x = x + 160;//регулируется выход за рамки экрана если еды много
+                if (x + 120 > Width)
+                {
+                    y = y + 180;
+                    x = 10;
+
+                    eda[i].labelPrice.Location = new Point(eda[i].picture.Location.X + 130, eda[i].picture.Location.Y + 150);
+                }
+
+                //----------------------------------------------------------------
+
+            }
+        }
+
+        //сортировка по цене и названию
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = 10;
+            int y = 10;
+            for (int i = 0; i < 5; i++)
+            {
+                eda[i].picture.Visible = true;
+
+                if (Poisk.Text != "" &&
+                    !eda[i].name.ToUpper().Contains(Poisk.Text.ToUpper()))//по названию
+                    eda[i].picture.Visible = false;
+
+                try
+                {
+                    if (price.Text != "" &&
+                    Convert.ToInt32(price.Text) <= eda[i].price)//по цене
+                        eda[i].picture.Visible = false;
+                }
+                catch (Exception) { }
+
+                if (eda[i].picture.Visible)
+                {
+                    eda[i].picture.Location = new Point(x, y);
+                    eda[i].labelName.Location = new Point(x, y + 150);
+                    eda[i].labelPrice.Location = new Point(x+100, y + 150);
+                    x = x + 160;
+                    if (x + 120 > Width)
+                    {
+                        y = y + 180;
+                        x = 10;
+                    }
+                }
+
+
+                eda[i].labelName.Visible = eda[i].picture.Visible;
+                eda[i].labelPrice.Visible = eda[i].picture.Visible;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+            ProductForm newForm = new ProductForm(pb.Tag.ToString());
+            newForm.Show();
+        }
+    }
+}
